3a9408d1da6005fe08543098ccd09003354821fe
Jason Noble <github+jasonn@jasonnoble.org>
2014-08-13 23:48:16 -0600

    End of class Wed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'cars#index'
 
   resources :cars
+
+  resources :users, only: [:new, :create],
+            path_names: { new: 'signup' }
+
+  get 'login' => 'sessions#login'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- spec/factories/cars.rb
+++ spec/factories/cars.rb
@@ -4,7 +4,7 @@ FactoryGirl.define do
   factory :car do
     make "MyString"
     model "MyString"
-    year 1
+    year { (1905..2014).to_a.sample }
     price "9.99"
   end
 end
